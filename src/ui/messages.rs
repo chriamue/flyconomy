b9f5f32eb617f2e330bfb8c80590634fcd91dd08
Christian M <chriamue@gmail.com>
2023-06-18 08:51:28 +0200

    :zap: improved performance of error messages

--- src/ui/messages.rs
+++ src/ui/messages.rs
@@ -9,28 +9,20 @@ impl Plugin for MessagesPlugin {
         app.add_system(show_error_messages);
     }
 }
-use std::collections::VecDeque;
 
 pub fn show_error_messages(mut contexts: EguiContexts, game_resource: Res<GameResource>) {
     let elapsed_time = game_resource.simulation.elapsed_time.as_secs_f64();
     let time_multiplier = game_resource.simulation.time_multiplier;
 
-    let mut relevant_messages = VecDeque::new();
-
-    for message in game_resource.simulation.error_messages.iter().rev() {
-        let message_timestamp = message.0;
-        if elapsed_time - message_timestamp < 5.0 * time_multiplier {
-            relevant_messages.push_front(message);
-        }
-    }
-
-    if !relevant_messages.is_empty() {
-        egui::Window::new("Error Messages")
+    if !game_resource.simulation.error_messages.is_empty() {
+        egui::Window::new("Log Book")
             .default_open(true)
             .show(contexts.ctx_mut(), |ui| {
-                for message in &relevant_messages {
-                    let text = format!("Timestamp: {:.2}, Error: {}", message.0, message.1);
-                    ui.label(text);
+                for message in &game_resource.simulation.error_messages {
+                    if elapsed_time - message.0 < 5.0 * time_multiplier {
+                        let text = format!("{:.0}, {}", message.0, message.1);
+                        ui.label(text);
+                    }
                 }
             });
     }
