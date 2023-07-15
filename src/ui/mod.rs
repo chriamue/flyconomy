212ee6296876d3df957f4d6ccd78a855e39c4478
Christian M <chriamue@gmail.com>
2023-07-15 19:40:35 +0200

    :lipstick: adds lightmode

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,4 +1,10 @@
-use bevy::prelude::{App, Plugin, States};
+use bevy::prelude::{App, IntoSystemConfigs, OnUpdate, Plugin, ResMut, Resource, States};
+use bevy_egui::{
+    egui::{vec2, Align2, Style, Visuals, Window},
+    EguiContexts,
+};
+
+use crate::game::GameState;
 
 mod aerodromes_ui;
 mod analytics_ui;
@@ -32,6 +38,12 @@ impl Plugin for UiPlugin {
         app.add_plugin(simulation_control::SimulationControlPlugin);
         app.add_plugin(analytics_ui::AnalyticsPlugin);
         app.add_plugin(office_ui::OfficePlugin);
+        app.insert_resource(StyleState { is_dark: true });
+        app.add_systems(
+            (style_switch_ui,)
+                .in_set(OnUpdate(GameState::Playing))
+                .in_set(OnUpdate(UiState::Settings)),
+        );
     }
 }
 
@@ -43,4 +55,26 @@ pub enum UiState {
     Analytics,
     Schedule,
     Office,
+}
+
+#[derive(Default, Resource)]
+pub struct StyleState {
+    pub is_dark: bool,
+}
+
+fn style_switch_ui(mut contexts: EguiContexts, mut style_state: ResMut<StyleState>) {
+    Window::new("Style Switch")
+        .anchor(Align2::LEFT_TOP, vec2(0.0, 100.0))
+        .default_open(true)
+        .show(contexts.ctx_mut(), |ui| {
+            let mut is_dark = style_state.is_dark;
+            if ui.checkbox(&mut is_dark, "Dark Mode").clicked() {
+                style_state.is_dark = is_dark;
+                if is_dark {
+                    ui.ctx().set_visuals(Visuals::dark());
+                } else {
+                    ui.ctx().set_visuals(Visuals::light());
+                }
+            }
+        });
 }
