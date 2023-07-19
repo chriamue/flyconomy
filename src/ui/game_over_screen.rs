9fdb1a5568e2e81c898eecd482346966465b0301
Christian M <chriamue@gmail.com>
2023-07-19 21:07:48 +0200

    :recycle::lipstick: change style of welcome and game over screen

--- src/ui/game_over_screen.rs
+++ src/ui/game_over_screen.rs
@@ -18,33 +18,37 @@ pub fn game_over_screen_system(
     mut game_state_next_state: ResMut<NextState<GameState>>,
 ) {
     egui::CentralPanel::default().show(contexts.ctx_mut(), |ui| {
-        ui.label("Game Over");
+        ui.vertical_centered(|ui| {
+            ui.label("");
+            ui.heading("Game Over!");
+            ui.label("");
 
-        ui.label(format!(
-            "Final Airline Value: ${:.2}",
-            game_resources
-                .simulation
-                .environment
-                .company_finances
-                .cash(game_resources.simulation.environment.timestamp)
-        ));
-        ui.label(format!(
-            "Total Planes: {}",
-            game_resources.simulation.environment.planes.len()
-        ));
+            ui.label(format!(
+                "Final Airline Value: ${:.2}",
+                game_resources
+                    .simulation
+                    .environment
+                    .company_finances
+                    .cash(game_resources.simulation.environment.timestamp)
+            ));
+            ui.label(format!(
+                "Total Planes: {}",
+                game_resources.simulation.environment.planes.len()
+            ));
 
-        ui.label("Thank you for playing Flyconomy!");
+            ui.label("Thank you for playing Flyconomy!");
 
-        if ui.button("Restart Game").clicked() {
-            game_resources.simulation = Simulation::new(
-                Default::default(),
-                Box::new(StringBasedWorldData::default()),
-            );
-            game_state_next_state.set(GameState::Welcome);
-        }
-        #[cfg(not(target_arch = "wasm32"))]
-        if ui.button("Quit").clicked() {
-            std::process::exit(0);
-        }
+            if ui.button("Restart Game").clicked() {
+                game_resources.simulation = Simulation::new(
+                    Default::default(),
+                    Box::new(StringBasedWorldData::default()),
+                );
+                game_state_next_state.set(GameState::Welcome);
+            }
+            #[cfg(not(target_arch = "wasm32"))]
+            if ui.button("Quit").clicked() {
+                std::process::exit(0);
+            }
+        });
     });
 }
--- src/ui/welcome_screen.rs
+++ src/ui/welcome_screen.rs
@@ -16,29 +16,37 @@ pub fn welcome_screen_system(
     mut game_state_next_state: ResMut<NextState<GameState>>,
 ) {
     egui::CentralPanel::default().show(contexts.ctx_mut(), |ui| {
-        ui.label("Welcome to Flyconomy!");
+        ui.vertical_centered(|ui| {
+            ui.label("");
+            ui.heading("Welcome to Flyconomy!");
+            ui.label("");
 
-        ui.label("Flyconomy is an economic simulation game where you manage an airline company.");
+            ui.label(
+                "Flyconomy is an economic simulation game where you manage an airline company.",
+            );
 
-        ui.label("Features:");
-        ui.label("- Choose your home aerodrome and buy your first aircraft.");
-        ui.label("- Plan flights by selecting an airplane and a destination aerodrome.");
-        ui.label("- Calculate the estimated distance and profit for your planned flights.");
-        ui.label("- Schedule your flights.");
+            ui.label("Features:");
+            ui.label("- Choose your home aerodrome and buy your first aircraft.");
+            ui.label("- Plan flights by selecting an airplane and a destination aerodrome.");
+            ui.label("- Calculate the estimated distance and profit for your planned flights.");
+            ui.label("- Schedule your flights.");
 
-        ui.label("Rules:");
-        ui.label("- Start by choosing a home aerodrome and buying your first aircraft.");
-        ui.label("- Plan flight routes between aerodromes.");
-        ui.label("- Monitor fuel prices and adapt your routes.");
-        ui.label("- Reinvest your profits to buy new planes and expand.");
+            ui.label("Rules:");
+            ui.label("- Start by choosing a home aerodrome and buying your first aircraft.");
+            ui.label("- Plan flight routes between aerodromes.");
+            ui.label("- Monitor fuel prices and adapt your routes.");
+            ui.label("- Reinvest your profits to buy new planes and expand.");
 
-        ui.label("Getting Started:");
-        ui.label("- Use the flight planning UI to select an airplane and a destination aerodrome.");
-        ui.label("- View the estimated distance and profit before scheduling your flight.");
-        ui.label("- Click on 'Plan Flight' to schedule your flight.");
+            ui.label("Getting Started:");
+            ui.label(
+                "- Use the flight planning UI to select an airplane and a destination aerodrome.",
+            );
+            ui.label("- View the estimated distance and profit before scheduling your flight.");
+            ui.label("- Click on 'Plan Flight' to schedule your flight.");
 
-        if ui.button("Start Game").clicked() {
-            game_state_next_state.set(GameState::Playing);
-        }
+            if ui.button("Start Game").clicked() {
+                game_state_next_state.set(GameState::Playing);
+            }
+        });
     });
 }
