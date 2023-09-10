1bd057a7784861fa6ac759897c4272824a461369
Christian M <chriamue@gmail.com>
2023-09-10 17:10:57 +0200

    :sparkles: adds attraction mapping

--- contracts/flyconomy_attractions/src/lib.rs
+++ contracts/flyconomy_attractions/src/lib.rs
@@ -5,11 +5,25 @@
 pub mod flyconomy_attractions {
     use openbrush::traits::Storage;
 
+    #[derive(scale::Decode, scale::Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    pub struct Attraction {
+        id: Id,
+        lat: i32, // multiplied by 10,000 for precision
+        lon: i32, // multiplied by 10,000 for precision
+        name: ink::prelude::string::String,
+        description: ink::prelude::string::String,
+    }
+
     #[derive(Default, Storage)]
     #[ink(storage)]
     pub struct Contract {
         #[storage_field]
         enumerable: enumerable::Data,
+        attractions: ink::storage::Mapping<Id, Attraction>,
     }
 
     impl Contract {
@@ -18,17 +32,92 @@ pub mod flyconomy_attractions {
         pub fn new() -> Self {
             Self::default()
         }
+
+        #[ink(message)]
+        pub fn mint(&mut self, account_id: AccountId, id: Id) -> Result<(), PSP34Error> {
+            psp34::Internal::_mint_to(self, account_id, id.clone())?;
+            self.attractions.insert(
+                id.clone(),
+                &Attraction {
+                    id: id,
+                    lat: 0,
+                    lon: 0,
+                    name: ink::prelude::string::String::from(""),
+                    description: ink::prelude::string::String::from(""),
+                },
+            );
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn set_location(&mut self, id: Id, lat: i32, lon: i32) -> Result<(), PSP34Error> {
+            let mut attraction = self.attractions.get(&id).ok_or(PSP34Error::TokenNotExists)?;
+            attraction.lat = lat;
+            attraction.lon = lon;
+            self.attractions.insert(id, &attraction);
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn get_location(&self, id: Id) -> Result<(i32, i32), PSP34Error> {
+            let attraction = self.attractions.get(&id).ok_or(PSP34Error::TokenNotExists)?;
+            Ok((
+                attraction.lat,
+                attraction.lon,
+            ))
+        }
     }
 
     #[cfg(test)]
     mod tests {
         use super::*;
         use crate::flyconomy_attractions::PSP34Impl;
+        use test_helpers::address_of;
 
         #[ink::test]
-        fn works() {
-            let contract = Contract::new();
+        fn mint_works() {
+            let mut contract = Contract::new();
             assert_eq!(PSP34Impl::total_supply(&contract), 0);
+            contract.mint(address_of!(Alice), Id::U8(1)).unwrap();
+            assert_eq!(PSP34Impl::total_supply(&contract), 1);
+        }
+
+        #[ink::test]
+        fn mint_existing_should_fail() {
+            let mut contract = Contract::new();
+            assert_eq!(PSP34Impl::total_supply(&contract), 0);
+            contract.mint(address_of!(Alice), Id::U8(1)).unwrap();
+            assert_eq!(PSP34Impl::total_supply(&contract), 1);
+            assert!(matches!(
+                contract.mint(address_of!(Alice), Id::U8(1)),
+                Err(_)
+            ));
+            assert_eq!(PSP34Impl::total_supply(&contract), 1);
+        }
+
+        #[ink::test]
+        fn attractions_works() {
+            let mut contract = Contract::new();
+            assert_eq!(PSP34Impl::total_supply(&contract), 0);
+            contract.mint(address_of!(Alice), Id::U8(1)).unwrap();
+            assert_eq!(PSP34Impl::total_supply(&contract), 1);
+            let attraction = contract.attractions.get(&Id::U8(1)).unwrap();
+            assert_eq!(attraction.lat, 0);
+            assert_eq!(attraction.lon, 0);
+            assert_eq!(attraction.name, "");
+            assert_eq!(attraction.description, "");
+        }
+
+        #[ink::test]
+        fn set_location_works() {
+            let mut contract = Contract::new();
+            assert_eq!(PSP34Impl::total_supply(&contract), 0);
+            contract.mint(address_of!(Alice), Id::U8(1)).unwrap();
+            assert_eq!(PSP34Impl::total_supply(&contract), 1);
+            contract.set_location(Id::U8(1), 1, 2).unwrap();
+            let (lat, lon) = contract.get_location(Id::U8(1)).unwrap();
+            assert_eq!(lat, 1);
+            assert_eq!(lon, 2);
         }
     }
 
@@ -346,6 +435,59 @@ pub mod flyconomy_attractions {
 
             assert_eq!(owners_token_by_index_1, Ok(psp34_id1.clone()));
             assert_eq!(owners_token_by_index_2, Err(PSP34Error::TokenNotExists));
+
+            Ok(())
+        }
+
+        #[ink_e2e::test]
+        async fn set_location_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            let constructor = ContractRef::new();
+            let address = client
+                .instantiate(
+                    "flyconomy_attractions",
+                    &ink_e2e::alice(),
+                    constructor,
+                    0,
+                    None,
+                )
+                .await
+                .expect("instantiate failed")
+                .account_id;
+
+            let psp34_id1 = Id::U8(1u8);
+
+            let mint_result_1 = {
+                let _msg = build_message::<ContractRef>(address.clone())
+                    .call(|contract| contract.mint(address_of!(Bob), psp34_id1.clone()));
+                client
+                    .call(&ink_e2e::alice(), _msg, 0, None)
+                    .await
+                    .expect("call failed")
+            }
+            .return_value();
+
+            assert_eq!(mint_result_1, Ok(()));
+
+            let set_location_result = {
+                let _msg = build_message::<ContractRef>(address.clone())
+                    .call(|contract| contract.set_location(psp34_id1.clone(), 1, 2));
+                client
+                    .call(&ink_e2e::alice(), _msg, 0, None)
+                    .await
+                    .expect("call failed")
+            }
+            .return_value();
+
+            assert_eq!(set_location_result, Ok(()));
+
+            let get_location_result = {
+                let _msg = build_message::<ContractRef>(address.clone())
+                    .call(|contract| contract.get_location(psp34_id1.clone()));
+                client.call_dry_run(&ink_e2e::alice(), &_msg, 0, None).await
+            }
+            .return_value();
+
+            assert_eq!(get_location_result, Ok((1, 2)));
 
             Ok(())
         }
